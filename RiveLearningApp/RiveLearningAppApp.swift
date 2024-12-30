1bdc21050588e463560b780a170ab16e0efc2ee3
Parth Darji <parthdarji2961@gmail.com>
2024-12-30 18:14:40 +0530

    Initial Commit

---
+++ RiveLearningApp/RiveLearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiveLearningAppApp.swift
+//  RiveLearningApp
+//
+//  Created by Parth Darji on 12/30/24.
+//
+
+import SwiftUI
+
+@main
+struct RiveLearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
