91701730d547cfc9552bd41473350b958f9b41db
Parth Darji <parthdarji2961@gmail.com>
2025-01-02 10:52:25 +0530

    Sign in screen in progress

---
+++ RiveLearningApp/SignInView.swift
@@ -0,0 +1,25 @@
+//
+//  SignInView.swift
+//  RiveLearningApp
+//
+//  Created by Parth Darji on 1/1/25.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text("Sign In")
+            .customFont(.largeTitle)
+        Text("Access to 240+ hours of content. Learn design and code, by building real apps with React and Swift.")
+            .customFont(.headline)
+        Divider()
+        Text("Sign up with Email, Apple or Google")
+            .customFont(.subheadline)
+            .foregroundColor(.secondary)
+    }
+}
+
+#Preview {
+    SignInView()
+}
