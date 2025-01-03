d88e863277c65de39a02394215c448069bab1a33
Parth Darji <parthdarji2961@gmail.com>
2025-01-03 16:24:54 +0530

    Sign in popup completed

---
+++ RiveLearningApp/Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Extensions.swift
+//  RiveLearningApp
+//
+//  Created by Parth Darji on 1/3/25.
+//
+
+import SwiftUI
+
+struct Extensions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Extensions()
+}
