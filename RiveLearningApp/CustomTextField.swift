17f6ad19d2c527d2c289b5c69a5a9c1f10e6194b
Parth Darji <parthdarji2961@gmail.com>
2025-01-03 13:24:03 +0530

    Textfield with its modifier common created

--- RiveLearningApp/CustomTextField.swift
+++ RiveLearningApp/CustomTextField.swift
@@ -7,12 +7,28 @@
 
 import SwiftUI
 
-struct CustomTextField: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct CustomTextField: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding(15)
+            .padding(.leading, 36)
+            .background(.white)
+            .mask(RoundedRectangle(cornerRadius: 10, style: .continuous))
+            .overlay(
+                RoundedRectangle(cornerRadius: 10, style: .continuous)
+                    .stroke()
+                    .fill(.black.opacity(0.1))
+            )
+            .overlay(
+                Image("Icon Email")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 8)
+            )
     }
 }
 
-#Preview {
-    CustomTextField()
+extension View{
+    func customTextField() -> some View{
+        modifier(CustomTextField())
+    }
 }
