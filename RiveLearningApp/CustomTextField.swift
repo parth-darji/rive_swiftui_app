0ae120afcac2537c93b050e3009c84d9e3f01f3e
Parth Darji <parthdarji2961@gmail.com>
2025-01-03 13:23:32 +0530

    Email password and login with apple/google ui designed

---
+++ RiveLearningApp/CustomTextField.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTextField.swift
+//  RiveLearningApp
+//
+//  Created by Parth Darji on 1/3/25.
+//
+
+import SwiftUI
+
+struct CustomTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTextField()
+}
