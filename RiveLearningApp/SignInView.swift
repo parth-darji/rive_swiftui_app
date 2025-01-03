faae3c40a1c4ee75763d9e84c13b147dcbdd2c69
Parth Darji <parthdarji2961@gmail.com>
2025-01-03 18:06:31 +0530

    Check animation implemented

--- RiveLearningApp/SignInView.swift
+++ RiveLearningApp/SignInView.swift
@@ -6,10 +6,13 @@
 //
 
 import SwiftUI
+import RiveRuntime
 
 struct SignInView: View {
     @State var email: String = ""
     @State var password: String = ""
+    @State var isLoading: Bool = false
+    let check = RiveViewModel(fileName: "check", stateMachineName: "State Machine 1")
     
     var body: some View {
         VStack(spacing: 24) {
@@ -34,15 +37,22 @@ struct SignInView: View {
                     .customTextField(image: Image("Icon Lock"))
             }
             
-            Label("Sign In", systemImage: "arrow.right")
-                .customFont(.headline)
-                .padding(20)
-                .frame(maxWidth: .infinity)
-                .background(Color(hex: "F77D8E"))
-                .foregroundColor(.white)
-                .cornerRadius(20, corners: [.topRight, .bottomLeft, .bottomRight])
-                .cornerRadius(8, corners: [.topLeft])
-                .shadow(color: Color(hex: "F77D8E").opacity(0.5), radius: 20, x: 0, y: 10)
+            Button {
+                isLoading = true
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1){
+                    check.triggerInput("Check")
+                }
+            } label: {
+                Label("Sign In", systemImage: "arrow.right")
+                    .customFont(.headline)
+                    .padding(20)
+                    .frame(maxWidth: .infinity)
+                    .background(Color(hex: "F77D8E"))
+                    .foregroundColor(.white)
+                    .cornerRadius(20, corners: [.topRight, .bottomLeft, .bottomRight])
+                    .cornerRadius(8, corners: [.topLeft])
+                    .shadow(color: Color(hex: "F77D8E").opacity(0.5), radius: 20, x: 0, y: 10)
+            }
             
             HStack {
                 Rectangle().frame(height: 1).opacity(0.1)
@@ -73,6 +83,16 @@ struct SignInView: View {
             RoundedRectangle(cornerRadius: 20, style: .continuous).stroke(.linearGradient(colors: [.white.opacity(0.8), .white.opacity(0.1)], startPoint: .topLeading, endPoint: .bottomTrailing))
         )
         .padding()
+        .overlay(
+            ZStack {
+                if(isLoading){
+                    check.view()
+                        .frame(width: 100, height: 100)
+                        .allowsHitTesting(false)
+                }
+            }
+           
+        )
     }
 }
 
