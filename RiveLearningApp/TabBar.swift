5cc9bf868006da362e1a7ece5ed7779cf0cfb644
Parth Darji <parthdarji2961@gmail.com>
2025-01-13 05:23:23 +0530

    Created tab items array for showing in the bottom tab

--- RiveLearningApp/TabBar.swift
+++ RiveLearningApp/TabBar.swift
@@ -31,3 +31,16 @@ struct TabBar: View {
 #Preview {
     TabBar()
 }
+
+struct TabItem: Identifiable {
+    var id = UUID()
+    var icon: RiveViewModel
+}
+
+var tabItems = [
+    TabItem(icon: RiveViewModel(fileName: "icons", stateMachineName: "CHAT_Interactivity", artboardName: "CHAT")),
+    TabItem(icon: RiveViewModel(fileName: "icons", stateMachineName: "SEARCH_Interactivity", artboardName: "SEARCH")),
+    TabItem(icon: RiveViewModel(fileName: "icons", stateMachineName: "TIMER_Interactivity", artboardName: "TIMER")),
+    TabItem(icon: RiveViewModel(fileName: "icons", stateMachineName: "BELL_Interactivity", artboardName: "BELL")),
+    TabItem(icon: RiveViewModel(fileName: "icons", stateMachineName: "USER_Interactivity", artboardName: "USER"))
+]
