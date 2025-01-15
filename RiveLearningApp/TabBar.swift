f4b92f7d6d59b7466c43c988e2f1bb03e9044e2c
Parth Darji <parthdarji2961@gmail.com>
2025-01-15 16:25:32 +0530

    Tabbar position changed to bottom center and made some ui fixes like padding overlay etc.

--- RiveLearningApp/TabBar.swift
+++ RiveLearningApp/TabBar.swift
@@ -12,21 +12,32 @@ struct TabBar: View {
     let icon = RiveViewModel(fileName: "icons", stateMachineName: "CHAT_Interactivity", artboardName: "CHAT")
     
     var body: some View {
-        HStack {
-            ForEach(tabItems) { item in
-                Button {
-                    item.icon.setInput("active", value: true)
-                    DispatchQueue.main.asyncAfter(deadline: .now()+1){
-                        item.icon.setInput("active", value: false)
+        VStack {
+            Spacer()
+            HStack {
+                ForEach(tabItems) { item in
+                    Button {
+                        item.icon.setInput("active", value: true)
+                        DispatchQueue.main.asyncAfter(deadline: .now()+1){
+                            item.icon.setInput("active", value: false)
+                        }
+                    } label: {
+                        item.icon.view()
+                            .frame(height: 36)
                     }
-                } label: {
-                    item.icon.view()
                 }
             }
+            .padding(12)
+            .background(Color("Background 2")
+                .opacity(0.8)
+            )
+            .background(.ultraThinMaterial)
+            .mask(RoundedRectangle(cornerRadius: 24, style: .continuous))
+            .shadow(color: Color("Background 2").opacity(0.3), radius: 20, x: 0, y: 20)
+            .overlay(RoundedRectangle(cornerRadius: 24, style: .continuous)
+                .stroke(.linearGradient(colors: [.white.opacity(0.5), .white.opacity(0)], startPoint: .topLeading, endPoint: .bottomTrailing)))
+            .padding(.horizontal, 24)
         }
-        .background(Color("Background 2")
-            .opacity(0.8)
-        )
     }
 }
 
