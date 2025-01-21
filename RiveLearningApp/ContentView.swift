62717ca7ab70475e94934c433e6b014a19acbb62
Parth Darji <parthdarji2961@gmail.com>
2025-01-21 16:16:38 +0530

    Menu animation added in home screen

--- RiveLearningApp/ContentView.swift
+++ RiveLearningApp/ContentView.swift
@@ -6,9 +6,12 @@
 //
 
 import SwiftUI
+import RiveRuntime
 
 struct ContentView: View {
     @AppStorage("selectedTab") var selectedTab: Tab = .chat
+    @State var isOpen = false
+    let button = RiveViewModel(fileName: "menu_button", stateMachineName: "State Machine", autoPlay: false)
     
     var body: some View {
         ZStack {
@@ -24,6 +27,16 @@ struct ContentView: View {
             case .user:
                 Text("User")
             }
+            button.view()
+                .frame(width: 44, height: 44)
+                .mask(Circle())
+                .shadow(color: Color("Shadow").opacity(0.2), radius: 5, x: 0, y: 5)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                .padding()
+                .onTapGesture {
+                    button.setInput("isOpen", value: isOpen)
+                    isOpen.toggle()
+                }
             TabBar()
         }
     
