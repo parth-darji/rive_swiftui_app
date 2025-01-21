97a0ec62048c4b82357731c6785222fb19d3648d
Parth Darji <parthdarji2961@gmail.com>
2025-01-21 17:45:10 +0530

    Safe area issue resolved in HomeView

--- RiveLearningApp/ContentView.swift
+++ RiveLearningApp/ContentView.swift
@@ -15,18 +15,27 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            switch selectedTab {
-            case .chat:
-                Text("Chat")
-            case .search:
-                Text("Search")
-            case .timer:
-                Text("Timer")
-            case .bell:
-                Text("Bell")
-            case .user:
-                Text("User")
+            Group {
+                switch selectedTab {
+                case .chat:
+                    HomeView()
+                case .search:
+                    Text("Search")
+                case .timer:
+                    Text("Timer")
+                case .bell:
+                    Text("Bell")
+                case .user:
+                    Text("User")
+                }
             }
+            .safeAreaInset(edge: .bottom){
+                Color.clear.frame(height: 80)
+            }
+            .safeAreaInset(edge: .top){
+                Color.clear.frame(height: 124)
+            }
+            .ignoresSafeArea()
             button.view()
                 .frame(width: 44, height: 44)
                 .mask(Circle())
@@ -39,7 +48,7 @@ struct ContentView: View {
                 }
             TabBar()
         }
-    
+        
     }
 }
 
