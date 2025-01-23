ee2e469c7410e7238b1aa2f6f5a385f4e5fa296b
Parth Darji <parthdarji2961@gmail.com>
2025-01-23 14:19:40 +0530

    OnboardingView accessed with person button at the top in HomeView

--- RiveLearningApp/ContentView.swift
+++ RiveLearningApp/ContentView.swift
@@ -11,6 +11,7 @@ import RiveRuntime
 struct ContentView: View {
     @AppStorage("selectedTab") var selectedTab: Tab = .chat
     @State var isOpen = false
+    @State var show = false
     let button = RiveViewModel(fileName: "menu_button", stateMachineName: "State Machine", autoPlay: false)
     
     var body: some View {
@@ -47,7 +48,23 @@ struct ContentView: View {
             .rotation3DEffect(.degrees(isOpen ? 30: 0), axis: (x: 0, y: -1, z: 0))
             .offset(x: isOpen ? 265 : 0)
             .scaleEffect(isOpen ? 0.9 : 1)
+            .scaleEffect(show ? 0.92 : 1)
             .ignoresSafeArea()
+            
+            Image(systemName: "person")
+                .frame(width: 36, height: 36)
+                .background(.white)
+                .mask(Circle())
+                .shadow(color: Color("Shadow").opacity(0.2), radius: 5, x: 0, y: 5)
+                .onTapGesture {
+                    withAnimation(.spring()){
+                        show = true
+                    }
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
+                .padding()
+                .offset(x: isOpen ? 100 : 0, y:4)
+            
             button.view()
                 .frame(width: 44, height: 44)
                 .mask(Circle())
@@ -64,6 +81,18 @@ struct ContentView: View {
                 }
             TabBar()
                 .offset(y: isOpen ? 300 : 0)
+                .offset(y: show ? 200 : 0)
+            
+            if show {
+                OnboardingView(show: $show)
+                    .background(.white)
+                    .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+                    .shadow(color: .black.opacity(0.5), radius: 40, x: 0, y: 40)
+                    .ignoresSafeArea(.all, edges: .top)
+                    .transition(.move(edge: .top))
+                    .offset(y: show ? -10: 0)
+                    .zIndex(1)
+            }
         }
         
     }
