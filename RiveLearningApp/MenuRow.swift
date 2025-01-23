65bb2d82c8a9c52a36a10922a38d988dc5d331c3
Parth Darji <parthdarji2961@gmail.com>
2025-01-23 12:17:54 +0530

    MenuRow component created to reduce the redundancy.

---
+++ RiveLearningApp/MenuRow.swift
@@ -0,0 +1,18 @@
+//
+//  MenuRow.swift
+//  RiveLearningApp
+//
+//  Created by Parth Darji on 1/23/25.
+//
+
+import SwiftUI
+
+struct MenuRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuRow()
+}
