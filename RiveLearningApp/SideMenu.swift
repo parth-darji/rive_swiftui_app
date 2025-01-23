78e6d8fdb9f118ca7511f716b9560dcedc9079d6
Parth Darji <parthdarji2961@gmail.com>
2025-01-23 11:55:41 +0530

    Side Menu Basic UI completed with icon animation

--- RiveLearningApp/SideMenu.swift
+++ RiveLearningApp/SideMenu.swift
@@ -9,8 +9,6 @@ import SwiftUI
 import RiveRuntime
 
 struct SideMenu: View {
-    let icon = RiveViewModel(fileName: "icons", stateMachineName: "HOME_interactivity", artboardName: "HOME")
-    
     var body: some View {
         VStack {
             HStack {
@@ -29,16 +27,37 @@ struct SideMenu: View {
             }
             .padding()
             
-            HStack {
-                icon.view()
-                    .frame(width: 32, height: 32)
-                    .opacity(0.6)
-                Text("Home")
-                    .customFont(.headline)
+            Text("BROWSE")
+                .customFont(.subheadline2)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal, 24)
+                .padding(.top, 40)
+                .opacity(0.7)
+            
+            VStack(alignment: .leading, spacing: 0) {
+                ForEach(menuItems) { item in
+                    Rectangle()
+                        .frame(height: 1)
+                        .opacity(0.1)
+                        .padding(.horizontal)
+                    HStack(spacing: 14) {
+                        item.icon.view()
+                            .frame(width: 32, height: 32)
+                            .opacity(0.6)
+                        Text(item.text)
+                            .customFont(.headline)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(12)
+                    .onTapGesture {
+                        item.icon.setInput("active", value: true)
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                            item.icon.setInput("active", value: false)
+                        }
+                    }
+                }
             }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(12)
-          
+            .padding(8)
             
             Spacer()
         }
@@ -53,3 +72,17 @@ struct SideMenu: View {
 #Preview {
     SideMenu()
 }
+
+struct MenuItem: Identifiable {
+    var id = UUID()
+    var text: String
+    var icon: RiveViewModel
+}
+
+var menuItems = [
+    MenuItem(text: "Home", icon: RiveViewModel(fileName: "icons", stateMachineName: "HOME_interactivity", artboardName: "HOME")),
+    MenuItem(text: "Search", icon: RiveViewModel(fileName: "icons", stateMachineName: "SEARCH_Interactivity", artboardName: "SEARCH")),
+    MenuItem(text: "Favorites", icon: RiveViewModel(fileName: "icons", stateMachineName: "STAR_Interactivity", artboardName: "LIKE/STAR")),
+    MenuItem(text: "Help", icon: RiveViewModel(fileName: "icons", stateMachineName: "CHAT_Interactivity", artboardName: "CHAT")),
+    
+]
