7f17a3a836ed3bec447a3ae4183ded897f23c5aa
Parth Darji <parthdarji2961@gmail.com>
2025-01-23 13:31:15 +0530

    HomeView and MenuView integrated with animations

--- RiveLearningApp/ContentView.swift
+++ RiveLearningApp/ContentView.swift
@@ -15,6 +15,14 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
+            Color("Background 2")
+                .ignoresSafeArea(edges: .all)
+            
+            SideMenu()
+                .opacity(isOpen ? 1 : 0)
+                .offset(x: isOpen ? 0 : -300)
+                .rotation3DEffect(.degrees(isOpen ? 0 : 30), axis: (x: 0, y: 1, z: 0))
+            
             Group {
                 switch selectedTab {
                 case .chat:
@@ -35,6 +43,10 @@ struct ContentView: View {
             .safeAreaInset(edge: .top){
                 Color.clear.frame(height: 124)
             }
+            .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .rotation3DEffect(.degrees(isOpen ? 30: 0), axis: (x: 0, y: -1, z: 0))
+            .offset(x: isOpen ? 265 : 0)
+            .scaleEffect(isOpen ? 0.9 : 1)
             .ignoresSafeArea()
             button.view()
                 .frame(width: 44, height: 44)
@@ -42,11 +54,16 @@ struct ContentView: View {
                 .shadow(color: Color("Shadow").opacity(0.2), radius: 5, x: 0, y: 5)
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
                 .padding()
+                .offset(x: isOpen ? 216 : 0)
                 .onTapGesture {
                     button.setInput("isOpen", value: isOpen)
-                    isOpen.toggle()
+                    withAnimation(.spring(response: 0.5, dampingFraction: 0.7)){
+                        isOpen.toggle()
+                    }
+                   
                 }
             TabBar()
+                .offset(y: isOpen ? 300 : 0)
         }
         
     }
